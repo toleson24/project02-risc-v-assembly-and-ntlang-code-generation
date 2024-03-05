31fc7018503aaf99789a374e0d4b1da34b3d62bf
againTraviscodes <t.uni.versity17@gmail.com>
2024-03-05 23:07:08 +0000

    pack_bytes_s implemented

--- pack_bytes_s.s
+++ pack_bytes_s.s
@@ -1,6 +1,19 @@
 .global pack_bytes_s
 
+# Pack 4 bytes into one 32 bit word (uint32_t).
+# Assume a3 is least significant byte.
+#
+# a0-a3 - single 4 byte words
+# t0 - uint32_t val
+
 pack_bytes_s:
-    # YOUR CODE HERE
-    ret
+    mv t0, a0
+	sllw t0, t0, 8
+	or t0, t0, a1
+	sllw t0, t0, 8
+	or t0, t0, a2
+	sllw t0, t0, 8
+	or t0, t0, a3
+	mv a0, t0
+	ret
 
