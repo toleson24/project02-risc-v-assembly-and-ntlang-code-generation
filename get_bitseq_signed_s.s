579aaadbcc631f40c4183725d078eb6d39b9f26b
againTraviscodes <t.uni.versity17@gmail.com>
2024-03-06 01:51:30 +0000

    primary logic coded for rstr_rec_s; TODO incestigate segfault

--- get_bitseq_signed_s.s
+++ get_bitseq_signed_s.s
@@ -1,6 +1,44 @@
 .global get_bitseq_signed_s
 .global get_bitseq_s
 
+# a0 - uint32_t num
+# a1 - int start
+# a2 - int end
+
+# t0 - int val_signed
+# t1 - int len = (end - start) + 1
+# t2 - int shift = 32 - len
+# t3 - int val
+
 get_bitseq_signed_s:
-    # YOUR CODE HERE
-    ret
+	sub t1, a2, a1
+	addi t1, t1, 1
+	li t3, 32
+	sub t2, t3, t1	
+
+	addi sp, sp, -64
+	sw ra, (sp)
+	sw a0, 8(sp)
+	sw a1, 16(sp)
+	sw a2, 24(sp)
+	sw t0, 32(sp)
+	sw t1, 40(sp)
+	sw t2, 48(sp)
+
+	call get_bitseq_s
+	mv t3, a0	
+	
+	lw ra, (sp)
+	lw a0, 8(sp)
+	lw a1, 16(sp)
+	lw a2, 24(sp)
+	lw t0, 32(sp)
+	lw t1, 40(sp)
+	lw t2, 48(sp)
+	addi sp, sp, 64
+	
+	sllw t3, t3, t2
+	srlw t0, t3, t2	
+
+	mv a0, t0
+	ret
--- rstr_rec_s.s
+++ rstr_rec_s.s
@@ -1,7 +1,88 @@
 .global rstr_rec_s
 
 # Reverse a string recursively
+#
+# a0 - char *dst
+# a1 - char *src
+
+# t0 - int src_len
+# t1 - int dst_len
 
 rstr_rec_s:
-    # YOUR CODE HERE
-    ret
+	addi sp, sp, -32
+	sw ra, (sp)
+	sw a0, 8(sp)
+	sw a1, 16(sp)
+
+	mv a0, a1
+	call strlen
+	mv a3, a0
+
+	lw ra, (sp)
+	lw a0, 8(sp)
+	lw a1, 16(sp)
+	addi sp, sp, 32
+
+	addi sp, sp, -64
+	sw ra, (sp)
+	sw a0, 8(sp)
+	sw a1, 16(sp)
+	sw a2, 24(sp)
+	sw a3, 32(sp)
+
+	mv a2, a1
+	li a1, 0
+	addi a3, a3, -1	
+	call basecase
+
+	lw ra, (sp)
+	lw a0, 8(sp)
+	lw a1, 16(sp)
+	lw a2, 24(sp)
+	lw a3, 32(sp)
+	addi sp, sp, 64
+	
+	j done
+
+basecase:
+	add t0, a2, a3
+	add t1, a0, a1
+	lb t2, (t0)
+	lb t3, (t1)
+	
+	bne t2, zero, recstep
+	sb x0, (t3)
+	
+	j done
+
+recstep:
+	sw t2, (t3)
+	
+	addi sp, sp, -80
+	sw ra, (sp)
+	sw a0, 8(sp)
+	sw a1, 16(sp)
+	sw a2, 24(sp)
+	sw a3, 32(sp)
+	sw t0, 40(sp)
+	sw t1, 48(sp)
+	sw t2, 56(sp)
+	sw t3, 64(sp)
+
+	addi a1, a1, 1
+	addi a3, a3, -1	
+	call basecase
+
+	lw ra, (sp)
+	lw a0, 8(sp)
+	lw a1, 16(sp)
+	lw a2, 24(sp)
+	lw a3, 32(sp)
+	lw t0, 40(sp)
+	lw t1, 48(sp)
+	lw t2, 56(sp)
+	lw t3, 64(sp)
+	addi sp, sp, 80
+
+done:
+	ret
