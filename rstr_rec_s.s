8a36d717dfc697db4da05450067d6376ea343428
againTraviscodes <t.uni.versity17@gmail.com>
2024-03-06 09:06:34 +0000

    rstr_rec_s implemented; segfaults after ret

--- rstr_rec_s.s
+++ rstr_rec_s.s
@@ -10,56 +10,60 @@
 
 rstr_rec_s:
 	addi sp, sp, -32
-	sw ra, (sp)
-	sw a0, 8(sp)
-	sw a1, 16(sp)
+	sd ra, (sp)
+	sd a0, 8(sp)
+	sd a1, 16(sp)
 
 	mv a0, a1
 	call strlen
 	mv a3, a0
 
-	lw ra, (sp)
-	lw a0, 8(sp)
-	lw a1, 16(sp)
+	ld ra, (sp)
+	ld a0, 8(sp)
+	ld a1, 16(sp)
 	addi sp, sp, 32
 
 	addi sp, sp, -64
-	sw ra, (sp)
-	sw a0, 8(sp)
-	sw a1, 16(sp)
-	sw a2, 24(sp)
-	sw a3, 32(sp)
+	sd ra, (sp)
+	sd a0, 8(sp)
+	sd a1, 16(sp)
+	sd a2, 24(sp)
+	sd a3, 32(sp)
 
 	mv a2, a1
 	li a1, 0
 	addi a3, a3, -1	
 	call basecase
 
-	lw ra, (sp)
-	lw a0, 8(sp)
-	lw a1, 16(sp)
-	lw a2, 24(sp)
-	lw a3, 32(sp)
-	addi sp, sp, 64
+	# lw ra, (sp)
+	# lw a0, 8(sp)
+	# lw a1, 16(sp)
+	# lw a2, 24(sp)
+	# lw a3, 32(sp)
+	# addi sp, sp, 64
 	
 	j done
+
 
 basecase:
-	add t0, a2, a3
-	add t1, a0, a1
-	lb t2, (t0)
-	lb t3, (t1)
+	add t0, a0, a1			# dst_idx
+	add t1, a2, a3			# src_idx
+	lw t2, (t0)				# dst[dst_idx]
+	lw t3, (t1)				# src[src_idx]
 	
-	bne t2, zero, recstep
-	sb x0, (t3)
+	# bne t3, zero, recstep
+	# bnez t3, recstep
+	bge a3, zero, recstep
+	sw x0, (t0)				# t3 ?
 	
 	j done
 
+
 recstep:
-	sw t2, (t3)
+	sw t3, (t0)
 	
 	addi sp, sp, -80
-	sw ra, (sp)
+	# sw ra, (sp)
 	sw a0, 8(sp)
 	sw a1, 16(sp)
 	sw a2, 24(sp)
@@ -73,7 +77,7 @@ recstep:
 	addi a3, a3, -1	
 	call basecase
 
-	lw ra, (sp)
+	# lw ra, (sp)
 	lw a0, 8(sp)
 	lw a1, 16(sp)
 	lw a2, 24(sp)
@@ -84,5 +88,13 @@ recstep:
 	lw t3, 64(sp)
 	addi sp, sp, 80
 
+
 done:
+	ld ra, (sp)
+	ld a0, 8(sp)
+	ld a1, 16(sp)
+	ld a2, 24(sp)
+	ld a3, 32(sp)
+	addi sp, sp, 64
+	
 	ret
