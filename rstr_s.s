8c02dc7d4b59b054e4e10c595063cdcf2a0557b8
againTraviscodes <t.uni.versity17@gmail.com>
2024-03-06 06:31:07 +0000

    rstr_s implemented; algorithm correct, memory address inaccessible

--- rstr_s.s
+++ rstr_s.s
@@ -5,28 +5,20 @@
 # Swap chars at indices a1 & a2 in array a0
 #
 # a0 - char arr[]
-# a1 - int i
-# a1 - int j
-# t0-t5 - char temp
+# a1 - char arr[]
+# a2 - int i
+# a3 - int j
+# t0-t3 - char temp
 
 swap_s:
- 	add t1, a1, a0
-	add t2, a2, a0 
-	#lb t3, (t1)		# arr[i]
-	#lb t4, (t2)		# arr[j]
+ 	add t0, a2, a0
+	add t1, a3, a1 
 
-	#mv t5, t3
-	#mv t3, t4
-	#mv t4, t5
-
-	#sb t3, (t1)
-	#sb t4, (t2)
-
+	lb t2, 0(t0)
 	lb t3, 0(t1)
-	lb t4, 0(t2)
 	
-	sb t4, 0(t1)
-	sb t3, 0(t2)
+	sb t3, 0(t0)
+	sb t2, 0(t1)
 
 	ret
 
@@ -39,46 +31,60 @@ swap_s:
 # t1 - int j
 
 rstr_s:
-	addi sp, sp, -16
+	addi sp, sp, -32
 	sw ra, (sp)
-	sb a0, 8(sp)	
-
+	sd a0, 8(sp)	
+	sd a1, 16(sp)
+	
+	mv a0, a1
 	call strlen
-	mv a1, a0 
+	mv t0, a0 
 
 	lw ra, (sp)
-	lb a0, 8(sp)
-	addi sp, sp, 16
+	ld a0, 8(sp)
+	ld a1, 16(sp)
+	addi sp, sp, 32
+	
+	addi sp, sp, -16
+	sw ra, (sp)
+	sw t0, 8(sp)	
 
-	mv t0, a1
 	addi t0, t0, -1
 	li t1, 0 
 	
 loop:
-	blt t0, t1, done
+	blt t0, x0, done
 
-	addi sp, sp, -48
+	addi sp, sp, -64
 	sw ra, (sp)
-	sb a0, 8(sp)
-	sb a1, 16(sp)
-	sb t0, 24(sp)
-	sb t1, 32(sp)
+	sd a0, 8(sp)
+	sd a1, 16(sp)
+	sd t0, 24(sp)
+	sd t1, 32(sp)
 
-	mv a1, t1
-	mv a2, t0
+	mv a2, t1
+	mv a3, t0
 	call swap_s	
 
 	lw ra, (sp)
-	lb a0, 8(sp)
-	lb a1, 16(sp)
-	lb t0, 24(sp)
-	lb t1, 32(sp)
-	addi sp, sp, 48
+	ld a0, 8(sp)
+	ld a1, 16(sp)
+	ld t0, 24(sp)
+	ld t1, 32(sp)
+	addi sp, sp, 64
 	
 	addi t0, t0, -1
 	addi t1, t1, 1
 	j loop		
 
 done:
+	lw ra, (sp)
+	lw t0, 8(sp)
+	li t3, 4
+	mul t0, t0, t3
+	add t1, a0, t0
+	sb zero, (t1)
+	addi sp, sp, 16
+
 	ret
     
