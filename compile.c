f08648f043b438c36b97d9289d4c787f899cb88b
againTraviscodes <t.uni.versity17@gmail.com>
2024-03-07 04:51:34 +0000

    compile.c code generation implemented using stack machine code process; data-driven appraoch retrieves operators

---
+++ compile.c
@@ -0,0 +1,102 @@
+/* compile.c - stack machine code generator */
+
+#include "ntlang.h"
+
+struct compile_oper_pair_st {
+	enum parse_oper_enum opid;
+	char *as_code;
+};
+
+char *as_oper_strings[] = COMPILE_OPER_STRINGS;
+/*	"addi t0, t0, t1\n",
+	"sub t0, t0, t1\n",
+	"mul t0, t0, t1\n",
+	"div t0, t0, t1\n",
+	"srlw t0, t0, t1\n",
+	"sraw t0, t0, t1\n",
+	"sllw t0, t0, t1\n",
+	"li t1, -1\nxor t0, t0, t1\n",	// TODO remove ?
+	"and t0, t0, t1\n",
+	"or t0, t0, t1\n",
+	"xor t0, t0, t1\n",
+	"",								// TODO remove ?
+};*/
+
+struct compile_oper_pair_st compile_oper_map[] = {
+	{OP_PLUS, "addi t0, t0, t1\n"},
+	{OP_MINUS, "sub t0, t0, t1\n"},/*
+	{OP_MULT, as_oper_strings[2]},
+	{OP_DIV, as_oper_strings[3]},
+	{OP_LSR, as_oper_strings[4]},
+	{OP_ASR, as_oper_strings[5]},
+	{OP_LSL, as_oper_strings[6]},
+	{OP_NOT, as_oper_strings[7]},	// TODO remove ?
+	{OP_AND, as_oper_strings[8]},
+	{OP_OR, as_oper_strings[9]},
+	{OP_XOR, as_oper_strings[10]},
+	{OP_NONE, as_oper_strings[11]},*/	// TODO remove ?
+};
+
+void generate_code(struct parse_node_st *pt);
+void generate_unary_oper_code(int oper);
+void generate_binary_oper_code(int oper);
+char * compile_oper_lookup(int opid);
+
+void generate_code(struct parse_node_st *pt) {
+	int sp_space = 4;
+	switch(pt->type) {
+		case EX_INTVAL:
+			printf("addi sp, sp, %d\n", -sp_space);
+			printf("li t0, %d\n", pt->intval.value);
+			printf("sw t0, (sp)\n");
+			break;
+		case EX_REG:
+			printf("addi sp, sp, %d\n", -sp_space);
+			printf("sw %s, (sp)\n", pt->reg.value);
+			break;
+		case EX_OPER1:
+			generate_code(pt->oper1.operand);
+			printf("lw t0, (sp)\n");
+			generate_unary_oper_code(pt->oper1.oper);
+			printf("sw t0, (sp)\n");
+			break;
+		case EX_OPER2:
+			generate_code(pt->oper2.left);
+			generate_code(pt->oper2.right);
+			printf("lw t1, (sp)\n");
+			printf("addi, sp, sp, %d\n", sp_space);
+			printf("lw t0, (sp)\n");
+			generate_binary_oper_code(pt->oper2.oper);
+			printf("sw t0, (sp)\n");
+			break;
+	}
+}
+
+void generate_unary_oper_code(int oper) {
+	if (oper == OP_MINUS) {
+		printf("neg t0, t0\n");
+	} else if (oper == OP_NOT) {
+		printf("li t1, -1\n");
+		printf("xor t0, t0, t1\n");
+	} else {
+		// TODO compile_error("Bad operator");
+	}
+}
+
+void generate_binary_oper_code(int oper) {
+	printf("%s\n", compile_oper_lookup(oper));
+}
+
+char * compile_oper_lookup(int opid) {
+	int len = *(&compile_oper_map + 1) - compile_oper_map;
+	char *as = "\n";
+
+	for (int i = 0; i < len; i++) {
+		if (opid == compile_oper_map[i].opid) {
+			as = compile_oper_map[i].as_code;
+			break;
+		}
+	}
+	
+	return as;
+}
