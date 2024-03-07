a6680d9f0462bd310d5d2b3e576bd774bc8e81d9
againTraviscodes <t.uni.versity17@gmail.com>
2024-03-07 08:25:48 +0000

    commented code removed

--- unpack_bytes_s.s
+++ unpack_bytes_s.s
@@ -7,21 +7,17 @@
 # t0 - int i
 # t1 - int len = 4
 # t2 - uint32_t temp
-# t3 - int mask = 0xFF = 255
 
 unpack_bytes_s:
     li t0, 0
 	li t1, 4
-	#mv t2, a0
-	#li t3, 255
 	
 loop:
 	bge t0, t1, done
-	# andi t2, t2, 0xFF
  	andi t2, a0, 0xFF
 	mul t4, t0, t1
 	add t4, a1, t4
-	sb t2, (t4)		# (a1)
+	sb t2, (t4)
 	sraw a0, a0, 8
 	addi t0, t0, 1
 	j loop
