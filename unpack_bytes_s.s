d60f87b6de4aa20d070967f0d1f06cb394dd9d81
againTraviscodes <t.uni.versity17@gmail.com>
2024-03-07 08:23:13 +0000

    unpack_bytes_s updated; correctly shift whole number (a0); 70/100

--- unpack_bytes_s.s
+++ unpack_bytes_s.s
@@ -12,16 +12,17 @@
 unpack_bytes_s:
     li t0, 0
 	li t1, 4
-	mv t2, a0
+	#mv t2, a0
 	#li t3, 255
 	
 loop:
 	bge t0, t1, done
-	andi t2, t2, 0xFF
- 	mul t4, t0, t1
+	# andi t2, t2, 0xFF
+ 	andi t2, a0, 0xFF
+	mul t4, t0, t1
 	add t4, a1, t4
 	sb t2, (t4)		# (a1)
-	srai t2, t2, 8
+	sraw a0, a0, 8
 	addi t0, t0, 1
 	j loop
 
